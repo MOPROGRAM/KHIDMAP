fb374dca5c284c8e0b80e0e7931f77f2c2176fcc
mo Business <mobusinessarena@gmail.com>
2025-06-21 13:23:31 +0000

    khidmap.web.app متاح ايضا عغلى فاير بيس

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getStorage, type FirebaseStorage } from 'firebase/storage';
 // These values are sourced from your .env file
 const firebaseConfigValues = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "khidmap.web.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
