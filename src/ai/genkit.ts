146f3ad3f75922ccfb9f0c71b1d6105029f34e29
MOPROGRAM <motwasel@yahoo.com>
2025-07-10 14:29:03 +0300

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: "AIzaSyAvnbo3SEVL8NNky4E3TD-HUlQhaMxQKT0", // Add your API key here
+      apiKey: "", // Add your API key here
     }),
   ],
   model: 'googleai/gemini-2.0-flash',
